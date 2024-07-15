7d5c01763f00017178c218beae7e3650c9978f91
Christian Guevara <chrisdb150@gmail.com>
2024-07-15 12:51:43 -0400

    init project

---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+import * as express from "express";
+
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+
+
+exports.api = onRequest(app);
