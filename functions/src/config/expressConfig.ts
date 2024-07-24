2a5ed3885a9fe60a3603519f4eaa376944be0646
Christian Guevara <chrisdb150@gmail.com>
2024-07-23 20:08:49 -0400

    adding cors

--- functions/src/config/expressConfig.ts
+++ functions/src/config/expressConfig.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AppRouter } from "../router";
+import cors from "cors";
 
 
 const app = express();
@@ -13,6 +14,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.status(200).send("Hey there!");
 });
+
+app.use(cors({
+    origin: true,
+}));
 
 // * Adding routes
 app.use(AppRouter.routes);
